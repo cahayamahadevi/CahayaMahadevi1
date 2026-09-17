c1de59923645fadaddbbcd8a59a7d99914ac0ca8
cahayamahadevi <cahayamahadevi@gmail.com>
2026-09-17 19:19:10 +0800

    Add files via upload

---
+++ soal_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int N;
+    long long A;
+    long long jumlah = 0;
+    
+    scanf ("%lld", &N);
+    
+    for (int i= 0; i < N; i++){
+        scanf ("%lld", &A);
+        jumlah += A;
+        
+    }   
+    if (jumlah < 0)
+    jumlah = -jumlah;
+    
+    printf ("%lld", jumlah);
+    printf ("\n");
+    return 0;
+}
